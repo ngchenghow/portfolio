f2edc1739cabde81db79d1ea2e3a5158de256912
ngchenghow <ngchenghow@gmail.com>
2018-01-26 00:23:35 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+
+var admin=express()
+
+admin.on('mount',function(){
+    console.log('admin mounted')
+})
+
+admin.get('/',function(req,res){
+    res.send('Admin Page');
+});
+
+app.get('/products', function (req, res) {
+    res.send('Product Page')
+})
+
+app.get('/', function (req, res) {
+    res.send('Portfolio Home Page')
+})
+
+app.use('/admin',admin);
+
+
+
+app.listen(8080)
+
