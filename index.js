a6d282b3b9a8ed393496162493e7ddb53cbac46a
ngchenghow <ngchenghow@gmail.com>
2018-01-26 16:47:04 +0800

    smtp-sever

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-var express = require('express')
+const express = require('express')
+const SMTPServer = require('smtp-server').SMTPServer;
+
+// This example starts a SMTP server using TLS with your own certificate and key
+const server = new SMTPServer({
+    size: 1024, // allow messages up to 1 kb
+    onRcptTo(address, session, callback) {
+        // do not accept messages larger than 100 bytes to specific recipients
+        let expectedSize = Number(session.envelope.mailFrom.args.SIZE) || 0;
+        if (address.address === 'almost-full@example.com' &&  expectedSize > 100) {
+            err = new Error('Insufficient channel storage: ' + address.address);
+            err.responseCode = 452;
+            return callback(err);
+        }
+        callback();
+    },
+    onData(stream, session, callback){
+        stream.pipe(process.stdout); // print message to console
+        stream.on('end', () => {
+            let err;
+            if(stream.sizeExceeded){
+                err = new Error('Message exceeds fixed maximum message size');
+                err.responseCode = 552;
+                return callback(err);
+            }
+            callback(null, 'Message queued as abcdef');
+        });
+    }
+});
+
+server.listen(465);
+
+
 var app = express()
 
 var admin=express()
