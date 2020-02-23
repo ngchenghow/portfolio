a429348249eaf41f711a15f9c2df4ab3c46206e1
ngchenghow <ngchenghow@gmail.com>
2020-02-23 19:17:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,11 @@
 <?php
 
- $dbuser = "root";
- $dbpass = "534521";
- $db = "root";
- $conn = new mysqli('34.68.98.47', $dbUser, $dbPass, $db, 3306);
-
- if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-/*
-$sql = "CREATE TABLE Website (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Table website created successfully";
-    } else {
-        echo "Error creating table: " . $conn->error;
-    }
-    
-    $conn->close();
-
-*/
-
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 
 
 ?>
