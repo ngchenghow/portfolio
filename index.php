63e5e363f0fcbb39e3fb4d8500ae1ded90cc5eeb
ngchenghow <ngchenghow@gmail.com>
2020-02-24 00:54:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('filebase1/src/Database.php');
+
 // setting the access and configration to your database
 $database = new \Filebase\Database([
     'dir' => '../filebase/database'
