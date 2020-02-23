1bf677da791bff8a4284b01b20e6a21e8a4c9e79
ngchenghow <ngchenghow@gmail.com>
2020-02-23 16:27:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo "hello world this is my portfolio website\n some update here.";
+
+$dbhost = "34.68.98.47";
+ $dbuser = "root";
+ $dbpass = "534521";
+ $db = "root";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+echo "connected";
+
 ?>
