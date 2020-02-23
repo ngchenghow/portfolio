b2ea72d000c7482e0b39ab59a49a813f83d90c90
ngchenghow <ngchenghow@gmail.com>
2020-02-23 17:03:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  $dbuser = "root";
  $dbpass = "534521";
  $db = "root";
- $conn = new mysqli('127.0.0.1', $dbUser, $dbPass, $db, 3306);
+ $conn = new mysqli('34.68.98.47', $dbUser, $dbPass, $db, 3306);
 
  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
